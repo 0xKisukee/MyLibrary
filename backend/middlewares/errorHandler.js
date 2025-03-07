982b38ac2fa0b99e70dfd4422f0e6a98ae990833
0xKisukee <aitbenali0502@gmail.com>
2025-03-07 21:03:17 +0100

    better error middleware

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,3 +1,12 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
 const errorHandler = (err, req, res, next) => {
     console.error(err);
     res.status(999).json({ message: "An error was caught by the error middleware." });
