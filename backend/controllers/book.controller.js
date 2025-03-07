359038419b054ab81becab20f042ab369d349a5c
0xKisukee <aitbenali0502@gmail.com>
2025-03-07 20:35:52 +0100

    controllers are now using the error middleware

---
+++ backend/controllers/book.controller.js
@@ -0,0 +1,14 @@
+// const bookService = require('../services/book.service.js');
+
+async function createBook(req, res, next) {
+    try {
+        const data = req.body;
+        res.send('Creating a book!');
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = {
+    createBook,
+};
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,14 @@
 // const userService = require('../services/user.service.js');
 
 async function createUser(req, res, next) {
-    const data = req.body;
-    res.send('Welcome on MyLibraaaaary!');
-    res.status(201).json(newUser);
+    try {
+        const data = req.body;
+        res.send('Creating a user!');
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {
     createUser,
-  };
+};
