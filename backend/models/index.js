9ddb4d7287745163559f40d1de0ee1435f948c0d
0xKisukee <aitbenali0502@gmail.com>
2025-03-10 10:10:28 +0100

    add many to many shelves association

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,6 +2,9 @@
 const User = require('./user.model');
 const Book = require('./book.model');
 
+User.belongsToMany(Book, { through: 'shelves' });
+Book.belongsToMany(User, { through: 'shelves' });
+
 // Export all models
 module.exports = {
   User,
