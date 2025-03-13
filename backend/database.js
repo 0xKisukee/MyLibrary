e3e15fe2a7ecaff2b122e17b8647bf201c2cafe5
0xKisukee <aitbenali0502@gmail.com>
2025-03-13 13:40:09 +0100

    update env variables for production

--- backend/database.js
+++ backend/database.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 // Connect to DB with Sequelize
 const sequelize = new Sequelize({
     dialect: 'postgres',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    database: process.env.POSTGRES_DB,
     logging: false
 });
 
