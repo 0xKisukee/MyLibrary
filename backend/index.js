a36f0241184d8a6c193ba94df62be3b5c77083f0
0xKisukee <aitbenali0502@gmail.com>
2025-03-13 15:44:19 +0100

    update variables for docker

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(errorHandler);
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server live on http://HOST:${PORT}.`);
+    console.log(`Server live on port ${PORT}.`);
 });
 
 // Create tables in database and admin user
